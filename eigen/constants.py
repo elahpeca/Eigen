3a3a8be9fe0700ce59f98030ca9f2643640eb181
elahpeca <acephaleee@gmail.com>
2024-07-28 14:27:12 +0300

    Hurray! First commit!

---
+++ eigen/constants.py
@@ -0,0 +1,21 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+rootdir = "/com/github/elahpeca/Eigen"
+app_id = "com.github.elahpeca.Eigen"
+pkgdatadir = "/home/acephale/Projects/Eigen/builddir/share/eigen"
+localedir = "/home/acephale/Projects/Eigen/builddir/share/locale"
+rel_ver = "1.0.0a"
+version = "1.0.0a"
+build_type = "debug"
